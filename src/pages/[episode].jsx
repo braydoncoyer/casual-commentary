ea4836c7783923422456abd0bd25eb09aacefd87
Braydon Coyer <braydon@coyer.net>
2022-07-19 22:08:39 -0500

    Styles for episode page

--- src/pages/[episode].jsx
+++ src/pages/[episode].jsx
@@ -1,11 +1,10 @@
-import { useMemo } from 'react'
-import Head from 'next/head'
-import { parse } from 'rss-to-json'
-
-import { useAudioPlayer } from '@/components/AudioProvider'
 import { Container } from '@/components/Container'
-import { PlayButton } from '@/components/player/PlayButton'
 import { FormattedDate } from '@/components/FormattedDate'
+import Head from 'next/head'
+import { PlayButton } from '@/components/player/PlayButton'
+import { parse } from 'rss-to-json'
+import { useAudioPlayer } from '@/components/AudioProvider'
+import { useMemo } from 'react'
 
 export default function Episode({ episode }) {
   let date = new Date(episode.published)
@@ -26,16 +25,16 @@ export default function Episode({ episode }) {
   return (
     <>
       <Head>
-        <title>{`${episode.title} - Their Side`}</title>
+        <title>{`${episode.title} - Casual Commentary`}</title>
         <meta name="description" content={episode.description} />
       </Head>
-      <article className="py-16 lg:py-36 bg-slate-900">
+      <article className="py-16 lg:py-36">
         <Container>
           <header className="flex flex-col">
             <div className="flex items-center gap-6">
               <PlayButton player={player} size="large" />
               <div className="flex flex-col">
-                <h1 className="mt-2 text-4xl font-bold text-slate-900">
+                <h1 className="mt-2 text-4xl font-bold text-slate-100">
                   {episode.title}
                 </h1>
                 <FormattedDate
@@ -44,13 +43,13 @@ export default function Episode({ episode }) {
                 />
               </div>
             </div>
-            <p className="mt-3 ml-24 text-lg font-medium leading-8 text-slate-700">
+            <p className="mt-3 ml-24 text-lg font-medium leading-8 text-slate-300">
               {episode.description}
             </p>
           </header>
           <hr className="my-12 border-gray-200" />
           <div
-            className="prose prose-slate mt-14 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2:nth-of-type(3n)]:before:bg-violet-200"
+            className="prose mt-14 text-slate-300 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-100 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2:nth-of-type(3n)]:before:bg-violet-200"
             dangerouslySetInnerHTML={{ __html: episode.content }}
           />
         </Container>
